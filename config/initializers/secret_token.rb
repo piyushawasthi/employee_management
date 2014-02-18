9be11b22e1d2e76e903d6daa4508a4b2368d82db
piyushanchanto <piyush.awasthi@anchanto.com>
2014-02-18 19:09:35 +0530

    Initital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ImployeeManagements::Application.config.secret_key_base = '076764252783c6d7143ae5a07831e6166d5979a02eba9095be8c014f7d69ebc25cc7a748cd5b0ec29185229cb21e4b748cf6c33e72f3ca2f8612f6c370c7cd83'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ImployeeManagements::Application.config.session_store :cookie_store, key: '_imployee_managements_session'
